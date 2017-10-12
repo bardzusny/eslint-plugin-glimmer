68b0ba4b62e5698e131f37f4f54660d049bd76f3
Adrian Zalewski <aazalewski@hotmail.com>
2017-10-13 00:44:42 +0200

    no-side-effects-in-tracking-getters: removing redundant suffix from error message.

--- tests/lib/rules/no-side-effects-in-tracking-getters.js
+++ tests/lib/rules/no-side-effects-in-tracking-getters.js
@@ -73,7 +73,7 @@ ruleTester.run("no-side-effects-in-tracking-getters", rule, {
             `,
             parser: 'typescript-eslint-parser',
             errors: [{
-                message: "Side effect in tracking getter property detected.",
+                message: "Side effect in tracking getter property",
                 line: 7,
             }]
         },
@@ -91,7 +91,7 @@ ruleTester.run("no-side-effects-in-tracking-getters", rule, {
             `,
             parser: 'typescript-eslint-parser',
             errors: [{
-                message: "Side effect in tracking getter property detected.",
+                message: "Side effect in tracking getter property",
                 line: 7,
             }]
         },
@@ -109,7 +109,7 @@ ruleTester.run("no-side-effects-in-tracking-getters", rule, {
             `,
             parser: 'typescript-eslint-parser',
             errors: [{
-                message: "Side effect in tracking getter property detected.",
+                message: "Side effect in tracking getter property",
                 line: 7,
             }]
         }
